46ecb06e274c385519b23ad7ee045708398ebd72
Denny Lianto <c14190004@john.petra.ac.id>
2022-03-05 13:45:48 +0700

    Update pert4_dart.dart

--- bin/pert4_dart.dart
+++ bin/pert4_dart.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 import 'dart:convert';
 
 void main() {
-  stdout.writeln("Masukkan N : ");
+  stdout.write("Masukkan N : ");
   String? n = stdin.readLineSync();
   int input = int.parse(n!);
 
@@ -12,16 +12,33 @@ void main() {
         input + 1, (i) => List.filled(input + 1, ' ', growable: false),
         growable: false);
     int num = 1;
+    int co = 0;
+    int swtch = -1;
 
     for (int i = 0; i < input; i++) {
       for (int j = 0; j < input; j++) {
-        if (j == (input / 2).floor() && i % 2 == 0) {
-          arr[i][j] = num.toString();
+        if (j >= (input / 2).floor() - co && j <= (input / 2).floor() + co) {
+          if (swtch == -1) {
+            arr[i][j] = num.toString();
+            swtch = 1;
+          } else {
+            arr[i][j] = ' ';
+            swtch = -1;
+          }
         } else {
           arr[i][j] = '*';
         }
       }
+      if (i >= (input / 2).floor()) {
+        co--;
+      } else {
+        co++;
+      }
+      swtch = -1;
       num++;
+      if (num > 9) {
+        num = 1;
+      }
     }
 
     for (int i = 0; i < input; i++) {

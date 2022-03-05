275d54a8022813458941c5ecc9e5ca98dea2aa15
Denny Lianto <c14190004@john.petra.ac.id>
2022-03-05 11:00:17 +0700

    Update pert4_dart.dart

--- bin/pert4_dart.dart
+++ bin/pert4_dart.dart
@@ -1,3 +1,12 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'dart:io';
+
+void main() {
+  stdout.writeln("Masukkan N : ");
+  String? n = stdin.readLineSync();
+  int input = int.parse(n!);
+
+  if (input % 2 == 0) {
+  } else {
+    for (int i = 0; i < input; i++) {}
+  }
 }

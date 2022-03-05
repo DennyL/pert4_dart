120091956460461c4b4f0fd4a4322b2eefd37d0c
Denny Lianto <c14190004@john.petra.ac.id>
2022-03-05 12:48:54 +0700

    Update pert4_dart.dart

--- bin/pert4_dart.dart
+++ bin/pert4_dart.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:convert';
 
 void main() {
   stdout.writeln("Masukkan N : ");
@@ -7,6 +8,27 @@ void main() {
 
   if (input % 2 == 0) {
   } else {
-    for (int i = 0; i < input; i++) {}
+    final arr = List.generate(
+        input + 1, (i) => List.filled(input + 1, ' ', growable: false),
+        growable: false);
+    int num = 1;
+
+    for (int i = 0; i < input; i++) {
+      for (int j = 0; j < input; j++) {
+        if (j == (input / 2).floor() && i % 2 == 0) {
+          arr[i][j] = num.toString();
+        } else {
+          arr[i][j] = '*';
+        }
+      }
+      num++;
+    }
+
+    for (int i = 0; i < input; i++) {
+      for (int j = 0; j < input; j++) {
+        stdout.write(arr[i][j]);
+      }
+      stdout.writeln();
+    }
   }
 }
